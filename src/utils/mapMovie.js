5b05ecbc1fa1a047ca13622d8d9eac791c38e5bf
Ruslan Reshetniak <reshetnyakrm@gmail.com>
2026-03-01 16:09:57 +0100

    01.03.2026 rreshetniak: developed mapMovie.js normalize function

--- src/utils/mapMovie.js
+++ src/utils/mapMovie.js
@@ -1,3 +1,29 @@
-export function mapMovie () {
+export function mapMovie (apiMovie = {}) {
+  const id = apiMovie.id ?? 
+    apiMovie.kinopoiskId ?? 
+    apiMovie.movieId ?? 
+    null;
+
+  const title = 
+    apiMovie.name ??
+    apiMovie.alternativeName ??
+    apiMovie.enName ??
+    apiMovie.title ??
+    "Untitled";
+
+  const year = apiMovie.year ?? "";
+
+  const ratingValue = 
+    apiMovie.rating?.kp ??
+    apiMovie.rating?.imdb ??
+    apiMovie.rating ??
+    null;
+
+  const rating = Number.isFinite(ratingValue) ? ratingValue : null;
+
+  const poster = 
+    apiMovie.poster?.previewUrl ??
+    apiMovie.poster?.url ??
+    "./src/assets/images/";
   
 }
